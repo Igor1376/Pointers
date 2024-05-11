ed27fcc13e70873b5986191b4c315b187ebd5dd2
Igor <bignick13@yandex.ru>
2024-05-11 14:59:31 +0300

    push_row_back

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -1,19 +1,38 @@
 #include<iostream>
 
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
 #define tab "\t"
 
-void FillRand(int arr[], const int n);
+int** Allocate(const int rows, const int cols);
+void Clear(int** arr, const int rows);
+
+void FillRand(int arr[], const int n, int minRand=0, int maxRand=100);
+void FillRand(int** arr, const int rows, const int cols);
+
 void Print(int arr[], const int n);
+void Print(int** arr, const int rows, const int cols);
 
 int* push_back(int arr[], int& n, int value);
 int* push_front(int arr[], int& n, int value);
 
 int* pop_back(int arr[], int& n);
 
+int** push_row_back(int** arr, int& rows, const int cols);
+
+//#define DYNAMIC_MEMORY_1
+//#define DYNAMIC_MEMORY_2
+#define PERFOMANCE_CHECK
+
+
 void main()
 {
 	setlocale(LC_ALL, "");
+
+#ifdef DYNAMIC_MEMORY_1
 	int n;
 	cout << "Введите размер массива: "; cin >> n;
 	int* arr = new int[n];
@@ -33,22 +52,111 @@ void main()
 	Print(arr, n);
 
 	delete[] arr;
+#endif // DYNAMIC_MEMORY_1
+
+#ifdef DYNAMIC_MEMORY_2
+
+	int rows;
+	int cols;
+	cout << "Введите количество строк: "; cin >> rows;
+	cout << "Введите количество элеметнов строк: "; cin >> cols;
+
+	int** arr = Allocate(rows, cols);
+
+	FillRand(arr, rows, cols);
+	Print(arr, rows, cols);
+
+	arr = push_row_back(arr, rows, cols);
+	FillRand(arr[rows - 1], cols, 100, 1000);
+	Print(arr, rows, cols);
+	
+	Clear(arr, rows);
+	
+#endif // DYNAMIC_MEMORY_2
+#ifdef PERFOMANCE_CHECK
+
+	int rows;
+	int cols;
+	cout << "Введите количество строк: "; cin >> rows;
+	cout << "Введите количество элеметнов строк: "; cin >> cols;
+
+	int** arr = Allocate(rows, cols);
+	cout << "Memory allocated" << endl;
+	system("PAUSE");
+
+	arr = push_row_back(arr, rows, cols);
+	cout << "Row added" << endl;
+	system("PAUSE");
+
+	Clear(arr, rows);
+	cout << "Memory clean" << endl;
+
+#endif // PERFOMANCE_CHECK
 
 }
-
-void FillRand(int arr[], const int n)
+int** Allocate(const int rows, const int cols)
 {
+	//1) создаем массив указателей:
+	int** arr = new int*[rows];
+	//2) выделяем память под строки:
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int[cols] {};
+	}
+	return arr;
+}
+
+void Clear(int** arr, const int rows)
+{
+	//1)Сначала удаляем строки:
+	for(int i = 0; i < rows; i++)
+	{
+		delete[] arr[i];
+	}
+	//2) Удаляем массив указателей:
+	delete[] arr;
+}
+
+void FillRand(int arr[], const int n, int minRand, int maxRand)
+{
+	//обращение к элементам массива через арифметику указателей и  оператор разыменования
 	for (int i = 0; i < n; i++)
 	{
-		*(arr + i) = rand() % 100; //обращение к элементам массива через арифметику указателей и  оператор разыменования
+		*(arr + i) = rand() % (maxRand - minRand) + minRand; 
 	}
 }
+
+void FillRand(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+		cout << endl;
+	}
+}
+
 void Print(int arr[], const int n)
 {
 
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << tab; // обращение к элементам массива через оператор индексирования - []
+	}
+	cout << endl;
+}
+
+void Print(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
 	}
 	cout << endl;
 }
@@ -89,5 +197,24 @@ int* pop_back(int arr[], int& n)
 	int* buffer = new int[--n];
 	for (int i = 0; i < n; i++)buffer[i] = arr[i];
 	delete[] arr;
+	return buffer;
+}
+
+int** push_row_back(int** arr, int& rows, const int cols)
+{
+	//1) Создаем буферный масси указателей нужного размера:
+	int** buffer = new int* [rows+1];
+
+	//2) Копируем строки из исходного массива в массив указателей:
+	for (int i = 0; i < rows; i++)buffer[i] = arr[i];
+
+	//3) Удалеяем исходный массив указателей:
+	delete[] arr;
+
+	//4) Создаем строку и добавляем ее в массив:
+	buffer[rows] = new int[cols] {};
+
+	//5) После добавления строки в массив, количество его строк увеличивается:
+	rows++;
 	return buffer;
 }
